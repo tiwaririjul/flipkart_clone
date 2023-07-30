5c52837a83db9a34c69a4fa8938efd6824ebb3fb
tiwaririjul <tiwaririjul7@gmail.com>
2023-07-30 13:51:27 +0530

    filter pagination

--- Flipkart_clone/client/src/components/category/Category.jsx
+++ Flipkart_clone/client/src/components/category/Category.jsx
@@ -1,26 +1,111 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
-import Product from './Product';
-import { useSelector, useDispatch } from "react-redux"; // hooks
-import { getMobileProducts } from '../../redux/actions/productActions';
-const Category = () => {
-    const { Mobiproducts } = useSelector((state) => state.getMobileProducts);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getMobileProducts());
-    }, [dispatch]);
-    return (
-        <div>
-          {Mobiproducts ? (
-            Mobiproducts.map((product) => {
-                return <Product key={product.id} product={product}/>
-            })
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
-      );
-    
-}
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Product from "./Product";
+import { useSelector, useDispatch } from "react-redux";
+import { getMobileProducts } from "../../redux/actions/productActions";
+import { Slider, Box, Typography, Button } from "@mui/material";
 
-export default Category
+const marks = [
+  {
+    value: 0,
+    label: "0",
+  },
+  {
+    value: 5000,
+    label: "5000",
+  },
+  {
+    value: 10000,
+    label: "10000",
+  },
+  {
+    value: 50000,
+    label: "50000",
+  },
+  {
+    value: 100000,
+    label: "100000",
+  },
+];
+
+const productsPerPage = 5; // Number of products to display per page
+
+const Category = () => {
+  const { Mobiproducts } = useSelector((state) => state.getMobileProducts);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMobileProducts());
+  }, [dispatch]);
+
+  const [priceRange, setPriceRange] = useState([0, 100000]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleSliderChange = (event, newValue) => {
+    setPriceRange(newValue);
+  };
+
+  const filteredProducts = Mobiproducts
+    ? Mobiproducts.filter(
+        (product) =>
+          product.price >= priceRange[0] && product.price <= priceRange[1]
+      )
+    : [];
+
+  // Calculate total number of pages based on the productsPerPage
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+
+  // Get the products to display for the current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = filteredProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  // Function to handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div>
+      <Box px={2}>
+        <Typography gutterBottom>Price Range</Typography>
+        <Slider
+          value={priceRange}
+          onChange={handleSliderChange}
+          valueLabelDisplay="auto"
+          min={0}
+          max={100000}
+          step={1000}
+          marks={marks}
+        />
+        <Typography>
+          Min Price: {priceRange[0]} INR | Max Price: {priceRange[1]} INR
+        </Typography>
+      </Box>
+      {currentProducts.length > 0 ? (
+        currentProducts.map((product) => {
+          return <Product key={product.id} product={product} />;
+        })
+      ) : (
+        <p>No products in the selected price range.</p>
+      )}
+      {/* Pagination */}
+      <Box mt={2} display="flex" justifyContent="center">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Button
+            key={index + 1}
+            variant={currentPage === index + 1 ? "contained" : "outlined"}
+            color="primary"
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </Button>
+        ))}
+      </Box>
+    </div>
+  );
+};
+
+export default Category;
--- Flipkart_clone/client/src/components/category/Product.jsx
+++ Flipkart_clone/client/src/components/category/Product.jsx
@@ -1,60 +1,68 @@
-import React from 'react'
+import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { Box, Typography, styled } from '@mui/material';
+import { Box, Typography, styled } from "@mui/material";
 const Component = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '10px 0 0 300px !important',
-    overflowX: 'overlay',
-    [theme.breakpoints.down('lg')]: {
-        margin: '0px !important'
-    }
-}))
+  display: "flex",
+  justifyContent: "space-between",
+  margin: "10px 0 0 300px !important",
+  overflowX: "overlay",
+  [theme.breakpoints.down("lg")]: {
+    margin: "0px !important",
+  },
+}));
 const Image = styled("img")({
-    width: "auto",
-    height: "180px",
-  });
-const Product = ({product}) => {
+  width: "auto",
+  height: "180px",
+});
+const Product = ({ product }) => {
   //console.log(product.img_url);
-    const fassured =
+  const fassured =
     "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png";
-    return (
-        <Link to={`mobileproduct/${product.product_id}`} style={{textDecoration:'none'}}>
-        <Component>
-            <Box style={{display:'flex' }}>
-                <Image src= {product.image_url} alt="" />
-                <Box style={{marginLeft:'10px'}}>
-                    <Typography>{product.model}</Typography>
-                    <Typography>Rating {product.rating}</Typography>
-                    <ul>
-                        <li>Ram {product.ram_capacity}</li>
-                        <li>Rom {product.internal_memory}</li>
-                        <li>Battery {product.battery_capacity} Mh</li>
-                        <li>Size : {product.screen_size}</li>
-                        <li> Network {product.has_5g?"5G" : "4G"}</li>
-                    </ul>
-                </Box>
+  return (
+    <Link
+      to={`mobileproduct/${product.product_id}`}
+      style={{ textDecoration: "none" }}
+    >
+      <Component>
+        <Box style={{ display: "flex" }}>
+          <Image src={product.image_url} alt="" />
+          <Box style={{ marginLeft: "10px" }}>
+            <Typography>{product.model}</Typography>
+            <Typography>Rating {product.rating}</Typography>
+            <ul>
+              <li>Ram {product.ram_capacity}</li>
+              <li>Rom {product.internal_memory}</li>
+              <li>Battery {product.battery_capacity} Mh</li>
+              <li>Size : {product.screen_size}</li>
+              <li> Network {product.has_5g ? "5G" : "4G"}</li>
+              <li> price {product.price}</li>
+            </ul>
+          </Box>
+        </Box>
+        <Box>
+          <Typography style={{ fontSize: 20 }}>{product.price} INR</Typography>
+          <Typography style={{ marginTop: 5, color: "#878787", fontSize: 14 }}>
+            Discount 1200 INR
+            <Box component="span">
+              <img
+                src={fassured}
+                style={{ width: 77, marginLeft: 20 }}
+                alt=""
+              />
             </Box>
-            <Box>
-                <Typography style={{ fontSize: 20 }}>{product.price} INR</Typography>
-                <Typography style={{ marginTop: 5, color: "#878787", fontSize: 14 }}>
-                  Discount 1200 INR
-                    <Box component="span">
-                        <img src={fassured} style={{ width: 77, marginLeft: 20 }} alt="" />
-                    </Box>
-                </Typography>
-                <Typography style={{ marginTop: 5, color: "#878787", fontSize: 14 }}>
-                    <Box component="span" style={{ fontSize: 28 }}>
-                        Free Delievery by Today
-                    </Box>
-                    &nbsp;&nbsp;&nbsp;
-                </Typography>
-                <Typography>Upto 1000Rs Off on Exchange</Typography>
-                <Typography>Bank Offer</Typography>
+          </Typography>
+          <Typography style={{ marginTop: 5, color: "#878787", fontSize: 14 }}>
+            <Box component="span" style={{ fontSize: 28 }}>
+              Free Delievery by Today
             </Box>
-        </Component>
-        </Link>
-    )
-}
+            &nbsp;&nbsp;&nbsp;
+          </Typography>
+          <Typography>Upto 1000Rs Off on Exchange</Typography>
+          <Typography>Bank Offer</Typography>
+        </Box>
+      </Component>
+    </Link>
+  );
+};
 
-export default Product
+export default Product;
